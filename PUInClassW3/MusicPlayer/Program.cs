40afca721f30e830fa5a2ff7c7a80ef4a158036d
SimonaIvanova7 <ivanova2701@abv.bg>
2018-10-16 18:33:53 +0300

    My new music player.

--- PUInClassW3/MusicPlayer/Program.cs
+++ PUInClassW3/MusicPlayer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,31 @@ namespace MusicPlayer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Do you want to play some music?");
+            string answer = Console.ReadLine();
+            
+           if (answer == "Yes")
+            {
+                Console.WriteLine("Playing music!");
+                using (SoundPlayer player = new SoundPlayer(@"C:\Users\fmi\Desktop\Week3\PU_IntroCSharp_InClass_1801681071\Music\MONKEY_SNAKE_ft._FANTASY_BAND_-_DA_ILI_NE_OFFICIAL_VIDEO_2018_--bcyknj7OZb4.wav"))
+                {
+                    // Use PlaySync to load and then play the sound.
+                    // ... The program will pause until the sound is complete.
+                    player.PlaySync();
+                }
+
+            }
+           else if (answer == "No")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+           else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+       
+
+                   //Console.WriteLine("Hello!");
         }
     }
 }
